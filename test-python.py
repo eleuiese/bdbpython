c240dfc48d996bb215576281cd438eaf6ff6191e
eleuiese <lcestradar@correo.udistrital.edu.co>
2022-08-15 16:23:20 -0500

    :fire: Syntax Error Done

--- test-python.py
+++ test-python.py
@@ -5,8 +5,12 @@
 # ** Date  : 18/05/2019       **
 # ** Author: ADL DevOps Team  **
 # ******************************
+
+import sys
+import os
 from sys import exit
-from test.main import run
+sys.path.insert(0, "/home/lucho/Escritorio/bdb/PythonExcercise/coding-test-python/test")
+from main import run
 
 if __name__ == "__main__":
     exit(run())
